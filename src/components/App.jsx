d295d1aa753d86c3beadddc09d9b34dfe39e6cdd
Qwerytx_ <potatoloadzch@gmail.com>
2024-08-05 06:01:40 +0700

    idk wtf im doin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import UpNavbar from './navigation/UpNavbar';
 import DownNavbar from './navigation/DownNavbar';
+import Card from './contents/card/Card'
 
 function App() {
   return (
     <div className='app'>
-      <UpNavbar />
-      <DownNavbar />
+      <div className="appNav" id='up'>
+        <UpNavbar />
+      </div>
+      <div className="appContent">
+        <Card />
+      </div>
+      <div className="appNav" id='down'>
+        <DownNavbar />
+      </div>
     </div>
   )
 }
