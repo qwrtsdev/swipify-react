4b0b44e1c90c01f93791c4a62977f3aa53b49177
Qwerytx_ <potatoloadzch@gmail.com>
2024-08-07 14:31:40 +0700

    fix: layout as intended

--- src/components/contents/card/Card.jsx
+++ src/components/contents/card/Card.jsx
@@ -10,19 +10,30 @@ import { faMusic } from '@fortawesome/free-solid-svg-icons'
 function Card() {
     return (
         <div className="card">
-            <div className="card-buttons">
-                <button>
-                    <FontAwesomeIcon icon={faClockRotateLeft} />
-                </button>
-                <button>
-                    <FontAwesomeIcon icon={faXmark} />
-                </button>
-                <button>
-                    <FontAwesomeIcon icon={faCheck} />
-                </button>
-                <button>
-                    <FontAwesomeIcon icon={faMusic} />
-                </button>
+            <div className="card-wrapper">
+                <div className="card-images">
+                    <img src="" alt="" />
+                </div>
+                <div className="card-interactive">
+                    <div className="interactive-info">
+                        <p>Song Name</p>
+                        <p>Artist Name</p>
+                    </div>
+                    <div className="interactive-buttons">
+                        <button>
+                            <FontAwesomeIcon icon={faClockRotateLeft} />
+                        </button>
+                        <button>
+                            <FontAwesomeIcon icon={faXmark} />
+                        </button>
+                        <button>
+                            <FontAwesomeIcon icon={faCheck} />
+                        </button>
+                        <button>
+                            <FontAwesomeIcon icon={faMusic} />
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
